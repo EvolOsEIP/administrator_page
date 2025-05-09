853ee9ebcbd38f0e0016a5fcdd55b4d6cf118072
Clément Lagier <clement.lagier@epitech.eu>
2025-05-09 11:51:48 +0900

    [UPDATE] Running the ModuleForm first instead of the CourseForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
-import Image from "next/image";
 
+import Image from "next/image";
 import CourseForm from './components/CourseForm';
+import ModuleForm from './components/ModuleForm';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-2xl font-bold mb-4">Course Registration</h1>
-      <CourseForm />
+         <ModuleForm />
       <p className="mt-4 text-gray-600">
         &copy; {new Date().getFullYear()} EvolOs. All rights reserved.
       </p>
