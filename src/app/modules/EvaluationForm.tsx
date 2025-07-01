b873eceb3c0188af59a10a5f1d90d25dd91fd00d
Clément Lagier <clement.lagier@epitech.eu>
2025-07-01 14:34:54 +0900

    [UPDATE] Moved the createEval function in a utils file

--- src/app/modules/EvaluationForm.tsx
+++ src/app/modules/EvaluationForm.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import EvalSteps from './EvalSteps';
 import EvalHeader from './EvalHeader';
 import SubmitButton from './SubmitButton';
-import getEvaluation from '../components/utils/eval';
+import { getEvaluation, createEval } from '../components/utils/eval';
 
 interface EvalutaionFormProps {
   moduleName: string;
@@ -16,92 +16,6 @@ const EvaluationForm = ({ moduleName, moduleId }: EvalutaionFormProps) => {
   const [steps, setSteps] = useState([
     { instruction: '', expectedAnswer: '', image: null as File | null, isOpen: true }
   ])
-
-  const createEval = async () => {
-    if (!title || !description || !moduleName) {
-      alert('Please fill in all fields');
-      return;
-    }
-    const evals = await getEvaluation();
-    console.log('Fetched evaluations:', evals.length);
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/modules/content`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
-      },
-      body: JSON.stringify({
-        "evaluations": [{
-            "evaluationIndex": evals.length + 1,
-            "moduleId": moduleId,
-            "title": title,
-            "assistantDiag": {},
-            "description": description,
-            "instruction": "Suivez les instructions ci-dessous pour compléter l'évaluation.",
-//            "Steps": [{
-//              "title": "step 1",
-//              "stepid": 1,
-//              "type": "evaluation",
-//              "widgets": {
-//                "actions": [
-//                  {
-//                    "data": "je veux ca et pas autre chose.",
-//                      "type": "input_text",
-//                      "description": "jattends que tu écrives ${expected_value} ici"
-//                    }
-//                  ],
-//                  "instructions": [{
-//                    "type": "image",
-//                    "description": "cette image montre un singe qui se pendouille 1",
-//                    "expected_value": "unNaffaire.png"
-//                  }
-//                ]
-//              },
-//              "instruction": "blabla 1"
-//            }]
-
-            "Steps": steps.map((step, index) => ({
-              title: `Step ${index + 1}`,
-              stepid: index + 1,
-              type: "evaluation",
-              widgets: {
-                actions: [
-                  {
-                    data: step.expectedAnswer,
-                    type: "input_text",
-                    description: `Please write your answer for step ${index + 1} here`,
-                    expected_value: step.expectedAnswer
-                  }
-                ],
-                instructions: step.image
-                  ? [
-                      {
-                        type: "image",
-                        description: step.instruction,
-                        expected_value: step.image.name
-                      }
-                    ]
-                  : []
-              },
-              instruction: step.instruction
-            }))
-          }]
-      }),
-    }).then((response) => {
-      if (!response.ok) {
-        alert('Failed to create evaluation. Please try again. ' + response.statusText);
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    }
-    ).catch((error) => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
-
-    if (res) {
-      alert('Evaluation created successfully!');
-    }
-  }
 
   return (
     <div className="flex items-center justify-center">
@@ -121,9 +35,7 @@ const EvaluationForm = ({ moduleName, moduleId }: EvalutaionFormProps) => {
           setSteps={setSteps}
         />
          <SubmitButton onClick={(e) => {
-           // avoid refreshing
-            e.preventDefault();
-          createEval()
+          createEval(moduleId, moduleName, title, description, steps)
         }}
         />
       </form>
